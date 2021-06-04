088026cd7c491ba8316659f20b2ae9435f3d96f5
jo-aguilar <joaoaguilar93@gmail.com>
2021-06-04 12:57:05 -0300

    Boletins majoritariamente implementados

--- notas.cpp
+++ notas.cpp
@@ -1,176 +1,362 @@
 #include <iostream>
+#include <unistd.h>
 #include <string>
 #include <cstring>
 #include <fstream>
 #include <vector>
+#include <dirent.h>
 #define DEBUG(X) std::cout << #X << std::endl
 
 namespace{
-   std::vector<std::string> linhas;
+	std::vector<std::string> linhas;
+	typedef struct {
+		unsigned int indice;
+		char marcador;
+		std::string alvo;
+		}Alvo;
 };
 
 void adicionar_boletim();
+void cria_boletim();
+void manipula_boletim();
+void mostra_boletim(std::vector<Alvo>);
+void refaz_boletim(std::string, std::vector<Alvo>);
+void mostra_lista_boletim();
+void remove_boletim();
+
+void adicionar_boletim(){
+	std::string comando;
+	std::cout << "Adicionar boletim ou manipular existente?\n"\
+		     "[cr]: criar               [ma]: manipular\n"\
+		     "[rm]: remover" 
+		 << std::endl << ">>";
+	std::cin >> comando;
+	if(comando=="cr" or comando=="criar")
+		cria_boletim();
+	else if(comando=="ma" or comando=="manipular")
+		manipula_boletim();
+	else if(comando=="rm" or comando=="remover")
+		remove_boletim();
+	else{
+		std::cout << "Comando não encontrado\nTerminando programa..." << std::endl;
+		exit(0);
+	}
+}
+
+void cria_boletim(){
+	/*Cria um novo boletim a partir de especificações dadas pelo usuário
+	** criar verificação de integridade de entrada a ser gravada, não 
+	   permitindo entrada de um boletim vazio, por exemplo*/
+	std::cout << "Criando um novo boletim" << std::endl;
+	std::vector<std::string> alvos_a_adicionar;
+	std::string alvo, nome_doc;
+	std::cout << "Nome do novo boletim (sem espaços): " << std::endl << ">>";
+	
+	std::cin >> nome_doc;
+	std::ofstream ofs = std::ofstream((nome_doc + ".txt").c_str());	
+	
+	getline(std::cin, alvo);
+	while(true){
+		std::cout << ">>[alvo]: ";
+		getline(std::cin, alvo);
+		if(alvo=="q")
+			break;
+		else{
+			ofs << "-" << std::endl;
+			ofs << alvo << std::endl;
+			alvos_a_adicionar.push_back(alvo);
+		}
+	}
+	for(int clk = 0; clk < alvos_a_adicionar.size(); clk++)
+		std::cout << alvos_a_adicionar[clk] << std::endl;
+	ofs.close();
+}
+void manipula_boletim(){
+	/*Entra em modo de manipulação de boletins, dando ao usuário as 
+	opções existentes para criação, remoção ou mostra de conteúdo dos
+	boletins já criados 
+	** função de remoção ainda precisa ser criada*/
+	std::cout << "Manipulando um boletim existente" << std::endl;
+	std::vector<Alvo>alvos;
+	int contador = 1, acerto;
+	std::string arquivo, leitura;
+	char opcao;
+	std::ifstream ifs;
+
+	mostra_lista_boletim();
+	std::cout << "Boletim a ser aberto (sem espaços): " << std::endl << ">>";
+	std::cin >> arquivo;
+	ifs = std::ifstream((arquivo + ".txt").c_str());
+	if(ifs.fail()){
+		std::cout << "Arquivo não pôde ser aberto\n"\
+			"Falha na abertura ou arquivo inexistente\n"\
+			"Terminando programa..." << std::endl;
+		exit(0);
+	}
+	while(true){
+		if(ifs.eof())
+			break;
+		else{
+			Alvo al;
+			al.indice = contador++;
+			getline(ifs, leitura); al.marcador = leitura[0]; 
+			getline(ifs, leitura); al.alvo = leitura; 
+			if(al.alvo.size()>0)
+				alvos.push_back(al);
+			}
+	}
+	ifs.close();
+	mostra_boletim(alvos);
+	std::cout << "Atirar em item? [S/n]\n" << ">>";
+	std::cin >> opcao;
+	if(opcao=='s' or opcao=='S'){
+		std::cout << "Item a ser atirado: \n" << ">>";
+		std::cin >> acerto;
+		if(acerto>0 and (acerto<alvos.size()+1)){
+			alvos[acerto-1].marcador = '*';
+			//mostra_alvos() mostra o vector, não o arquivo	
+			refaz_boletim(arquivo, alvos);
+			mostra_boletim(alvos);
+			}
+		else{
+			std::cout << "Índice inexistente\nTerminando..." << std::endl;
+			exit(0);
+			}
+	}
+	else if(opcao=='N' or opcao=='N'){;;}
+	
+}
+
+void mostra_boletim(std::vector<Alvo>alvos){
+	/*Mostra o conteúdo de um boletim previamente selecionado pelo usuário
+	de acordo com o conteúdo já adicionado em um vector de leitura de conteúdo
+	pré-formatado */
+	for(int clk = 0; clk < alvos.size(); clk++){
+		if(alvos[clk].marcador=='-')
+			std::cout << " ";
+		else
+			std::cout << alvos[clk].marcador;
+		std::cout << " ";
+		std::cout << "#" << alvos[clk].indice << "|"\
+		<< alvos[clk].alvo << std::endl << std::endl;
+	}
+}
+
+
+void refaz_boletim(std::string nome, std::vector<Alvo>alvos){
+	/*Refaz o conteúdo de um boletim modificado pelo usuário de acordo
+	com o novo conteúdo especificado por ele */
+	std::ofstream ofs = std::ofstream((nome + ".txt").c_str());
+	for(int clk = 0; clk < alvos.size(); clk++){
+		ofs << alvos[clk].marcador << std::endl;
+		ofs << alvos[clk].alvo << std::endl;
+		}
+	ofs.close();
+}
+
+void mostra_lista_boletim(){
+	/*Mostra os arquivos presentes no diretório de boletins diretamente
+	para que o usuário possa escolher entre eles
+	*Um diretório exclusivo para os boletins precisa ser feito */
+	DIR* dir;
+	struct dirent* leitura;
+	std::vector<char*> arquivos;
+	std::cout << "Boletins existentes: " << std::endl;
+	if((dir = opendir("."))!=nullptr){
+		while((leitura = readdir(dir))!=nullptr)
+			arquivos.push_back(leitura->d_name);
+		closedir(dir);
+	}
+	else{
+		perror("Sem boletins a serem abertos.\nFinalizando...\n");
+		return;
+	}
+	for(auto arquivo:arquivos)
+		std::cout << arquivo << std::endl;
+	std::cout << std::endl;
+}
+
+void remove_boletim(){
+	std::cout << "Removendo boletim existente" << std::endl;
+	std::string arquivo;
+	mostra_lista_boletim();
+	std::cout << "Qual boletim deve ser removido?:" << std:: endl;
+	std::cout << ">>";
+	std::cin >> arquivo;
+	if(remove((arquivo +".txt").c_str())==false)
+		std::cout << "Boletim removido." << std::endl;
+	else{
+		std::cout << "Arquivo não encontrado. \nTerminando..." << std::endl;
+		exit(0);
+	}
+}
+
+
+
+//-----------------------
 void adicionar_nota();
 bool testa_entrada(std::string);
 void mostra_notas();
 void remove_nota();
 void refaz_notas();
+void mostra_ajuda(){
+	std::cout << "Uso: ./notas <arg1> <arg2>\n"\
+		"                ./notas  <arg1> <arg2> <arg3>\n"\
+		"arg1 arg2 = acd nota  : adicionar nova nota\n"\
+		"arg1 arg2 = acd boletim: adicionar lista-boletim\n"\
+		"arg1 arg2 = lista notas  : ver todas as notas\n"\
+		"arg1 arg2 = lista boletim: ver boletins por nome \n"\
+		"arg1 arg2 arg3 = rm nota #[n] : apagar nota n" << std::endl;
+}
 
 //===============================================================================//
 //                                  MAIN                                         //
 //===============================================================================//
 int main(int argc, char** argv){
-   if(argc==1)
-      mostra_notas();
-   else if(argc==2){
-      if(argv[1]==std::string("ajuda")){
-         std::cout << "Uso: <notas arg1 arg2>\n"\
-                      "     <notas arg1 arg2 arg3>\n"\
-                      "arg1 arg2 = acd nota  : adicionar nova nota\n"\
-                      "arg1 arg2 = acd boletim: adicionar lista-boletim\n"\
-                      "arg1 arg2 = lista notas  : ver todas as notas\n"\
-                      "arg1 arg2 = lista boletim: ver boletins por nome \n"\
-                      "arg1 arg2 arg3 = rm nota #[n] : apagar nota n" << std::endl;
-      }
-   }
-   else if (argc==3){
-      if(argv[1]==std::string("adc")){
-         if(argv[2]==std::string("nota")){
-            adicionar_nota();
-         }
-         else if(argv[2]==std::string("boletim")){
-            adicionar_boletim();
-         }
-      }
-      else if(argv[1]==std::string("rm")){
-         if(argv[2]==std::string("nota"))
-            remove_nota();
-      }
-   }
-   else{
-      std::cout << "ERRO: Quantidade incompatível de comandos" << std::endl;
-   }
-   //fim de ifs()
+	if(argc==1){
+		std::cout << "Para uma listagem dos comandos existentes\n"\
+		"digite " << argv[0] << " ajuda" << std::endl;
+	}
+	if(argc==1)
+		mostra_notas();
+	else if(argc==2){
+		if(argv[1]==std::string("ajuda"))
+			mostra_ajuda();
+	}
+	else if (argc==3){
+		if(argv[1]==std::string("adc")){
+			if(argv[2]==std::string("nota"))
+				adicionar_nota();
+			else if(argv[2]==std::string("boletim"))
+				adicionar_boletim();
+		}
+		else if(argv[1]==std::string("rm")){
+			if(argv[2]==std::string("nota"))
+				remove_nota();
+		}
+	}
+	else
+		std::cout << "ERRO: Quantidade incompatível de comandos" << std::endl;
+	//fim de ifs()
 }
 //===============================================================================//
 //                                  FIM DE MAIN                                  //
 //===============================================================================//
 
 void refaz_notas(){
-//refaz a lista com as notas disponíveis no vetor geral
-//de linhas disponíveis
-   std::ofstream notas = std::ofstream("notas.txt");
-   for(int clk = 0; clk < linhas.size(); clk++)
-      notas << linhas[clk] << "\n*\n";
-   notas.close();
+	//refaz a lista com as notas disponíveis no vetor geral
+	//de linhas disponíveis
+	std::ofstream notas = std::ofstream("notas.txt");
+	for(int clk = 0; clk < linhas.size(); clk++)
+		notas << linhas[clk] << "\n*\n";
+	notas.close();
 }
 
 void remove_nota(){
-   //Remove uma nota existente na lista de acordo com um
-   //índice fornecido pelo usuário
-   //Não toma qualquer ação caso o índice seja menor que notas
-   //Não toma qualquer ação caso não existam notas a sere 
-   //removidas
-   std::ifstream notas = std::ifstream("notas.txt");
-   std::string leitura;
-   unsigned int contador = 0;
-   unsigned int indice;
-   std::cout << "Apagar nota número\n>>";
-   std::cin >>  indice;
-   if(!notas){
-      std::cout << "Não existe arquivo de notas a ser lido.\nEncerrando...\n";
-      return;
-   }
-   while(getline(notas, leitura)){
-      if(leitura==std::string("*"))
-         continue;
-      else
-         linhas.push_back(leitura);
-   }
-   
-   if(linhas.size()==0){
-      std::cout << "Não existem notas a serem apagadas.\nEncerrando..." << std::endl;
-      notas.close();
-      return;
-   }
-   else if(linhas.size()!=0){
-      if(linhas.size()<indice){
-      std::cout << "Índice maior que quantidade de notas.\nEncerrando..." << std::endl;
-      notas.close();
-      return;
-      }
-   }
-   notas.close();
-   linhas.erase(linhas.begin() + indice-1);
-   refaz_notas();
+	//Remove uma nota existente na lista de acordo com um
+	//índice fornecido pelo usuário
+	//Não toma qualquer ação caso o índice seja menor que notas
+	//Não toma qualquer ação caso não existam notas a sere 
+	//removidas
+	std::ifstream notas = std::ifstream("notas.txt");
+	std::string leitura;
+	unsigned int contador = 0;
+	unsigned int indice;
+	std::cout << "Apagar nota número\n>>";
+	std::cin >>  indice;
+	if(!notas){
+		std::cout << "Não existe arquivo de notas a ser lido.\nEncerrando...\n";
+		return;
+	}
+	while(getline(notas, leitura)){
+		if(leitura==std::string("*"))
+			continue;
+		else
+			linhas.push_back(leitura);
+	}
+
+	if(linhas.size()==0){
+		std::cout << "Não existem notas a serem apagadas.\nEncerrando..." << std::endl;
+		notas.close();
+		return;
+	}
+	else if(linhas.size()!=0){
+		if(linhas.size()<indice){
+			std::cout << "Índice maior que quantidade de notas.\nEncerrando..." << std::endl;
+			notas.close();
+			return;
+		}
+	}
+	notas.close();
+	linhas.erase(linhas.begin() + indice-1);
+	std::cout << "Nota #" << indice << " apagada..." << std::endl;
+	refaz_notas();
 }
 
 void mostra_notas(){
-//mostra as notas em formato amigável para o usuário
-//com a numeração posicional de cada nota nas lista
-   std::ifstream notas = std::ifstream("notas.txt");
-   std::string leitura;
-   unsigned int contador = 0;
-   if(!notas){
-      std::cout << "Não existe arquivo de notas a ser lido.\nEncerrando...\n";
-      return;
-   }
-   while(getline(notas, leitura)){
-      if(leitura==std::string("*"))
-         std::cout << "____________________________________" << std::endl;
-      else{
-         std::cout << "#" << ++contador << "| " << leitura << std::endl;
-      }
-   }
-   notas.close();
+	//mostra as notas em formato amigável para o usuário
+	//com a numeração posicional de cada nota nas lista
+	std::ifstream notas = std::ifstream("notas.txt");
+	std::string leitura;
+	unsigned int contador = 0;
+	if(!notas){
+		std::cout << "Não existe arquivo de notas a ser lido.\nEncerrando...\n";
+		return;
+	}
+	while(getline(notas, leitura)){
+		if(leitura==std::string("*"))
+			std::cout << "____________________________________" << std::endl;
+		else{
+			std::cout << "#" << ++contador << "| " << leitura << std::endl;
+		}
+	}
+	notas.close();
 
 }
 
 void adicionar_nota(){
-//adiciona uma nota nova ao documento geral de notas, criando
-//um documento para isso, caso nenhum documento de notas exista
-   unsigned int quantidade;
-   std::string entrada;
-   std::ofstream notas;
-   if(!std::ifstream("notas.txt")){
-      std::cout << "Não existe arquivo de notas.\nCriando...\n" << std::endl;
-      notas = std::ofstream("notas.txt");
-      notas.close();
-      }
-   
-   notas = std::ofstream("notas.txt", std::ios_base::app);
-   std::cout << "nota\n>> ";
-   getline(std::cin, entrada);
-   if(testa_entrada(entrada)==false){
-      std::cout << "Nenhuma nota adicionada.\nEncerrando..." << std::endl;
-      return;
-      }
-   else{
-      notas << entrada << "\n*" << std::endl;
-      notas.close();
-      return;
-   }
-   notas.close(); 
+	//adiciona uma nota nova ao documento geral de notas, criando
+	//um documento para isso, caso nenhum documento de notas exista
+	unsigned int quantidade;
+	std::string entrada;
+	std::ofstream notas;
+	if(!std::ifstream("notas.txt")){
+		std::cout << "Não existe arquivo de notas.\nCriando...\n" << std::endl;
+		notas = std::ofstream("notas.txt");
+		notas.close();
+	}
+
+	notas = std::ofstream("notas.txt", std::ios_base::app);
+	std::cout << "nota\n>> ";
+	getline(std::cin, entrada);
+	if(testa_entrada(entrada)==false){
+		std::cout << "Nenhuma nota adicionada.\nEncerrando..." << std::endl;
+		return;
+	}
+	else{
+		notas << entrada << "\n*" << std::endl;
+		notas.close();
+		std::cout << "Nota salva..." << std::endl;
+		return;
+	}
+	notas.close(); 
 }
 
 bool testa_entrada(std::string entrada){
-//verifica se a entrada dada pelo usuário como uma nova nota se
-//enquadra em um formato válido de nota, i.e, não é apenas uma
-//string com espaços vazios ou uma string de tamanho 0
-   if(entrada.size()==0)
-   //se a entrada for vazia
-      return false;
-   else{
-      for(int clk = 0; clk < entrada.size(); clk++){
-         if(entrada[clk]!=' ');
-         //se algum caractere for diferente de espaço em branco
-            return true;
-      }
-   }
-   //todos os caracteres são espaços em branco
-   return false;
+	//verifica se a entrada dada pelo usuário como uma nova nota se
+	//enquadra em um formato válido de nota, i.e, não é apenas uma
+	//string com espaços vazios ou uma string de tamanho 0
+	if(entrada.size()==0)
+		//se a entrada for vazia
+		return false;
+	else{
+		for(int clk = 0; clk < entrada.size(); clk++){
+			if(entrada[clk]!=' ');
+			//se algum caractere for diferente de espaço em branco
+			return true;
+		}
+	}
+	//todos os caracteres são espaços em branco
+	return false;
 }
 
-void adicionar_boletim(){
-   std::cout << "Adicionando boletim" << std::endl;
-}
